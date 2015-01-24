e22cd1d07fe526f4b283bc83429c899649e68a44
slimshayney <slim_shayney@yahoo.com>
2015-01-24 22:36:25 +0200

    final

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,35 @@
-## This function (makeCacheMatrix) will return the inverse of a matrix stored in another function (cacheSolve)
-## The below 2x2 matrix was created to test the function 
-## x <- matrix(c(3, 2, -7, -5), nrow = 2, ncol = 2)
-## The inverse of the above matrix can be computed as follows:
-## solve(x)
+## This function creates an object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) { ## This function sets the value of the matrix and the inverse
+makeCacheMatrix <- function(x = matrix()) {  
   z <- NULL
-  set <- function(y) {
-    x <<- y
+  set <- function(y) { ## sets the matrix
+    x <<- y ## assigns a value to an object in a state that is different from the current state
     z <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) z <<- inverse
-  getinverse <- function() z
+  get <- function() x ## gets the matrix
+  setinverse <- function(inverse) z <<- inverse ## sets the inverse
+  getinverse <- function() z ## gets the inverse
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-cacheSolve <- function(x, ...) {
-  z <- x$getinverse() 
-  if(!is.null(z)) {
-    message("getting cached data.")
-    return(z)
+## returns the inverse of the original matrix
+
+cacheSolve <- function(x, ...) { 
+  z <- x$getinverse() ## fetch the inversed matrix from the previous function (makeCacheMatrix) 
+  if(!is.null(z)) { ## if the inversed matrix is not null (empty) - in other words if it has already been computed  
+    return(z) ## then return the cached inversed matrix
   }
   data <- x$get()
-  z <- solve(data)
-  x$setinverse(z)
+  z <- solve(data) ## otherwise compute the inversed matrix
+  x$setinverse(z) ## then store it in the cache function (floating variable)
   z
 }
 
 To initiate the experiment type:
 
-x <- matrix(c(3, 2, -7, -5), nrow = 2, ncol = 2)
-m = makeCacheMatrix(x)
+x <- matrix(c(3, 2, -7, -5), nrow = 2, ncol = 2) ## creates a 2x2 matrix
+m = makeCacheMatrix(x) 
 m$get()
 
-cacheSolve(m)
+cacheSolve(m) ## remember to run this twice; the first time to compute the inversed matrix and the second time to fetch it from cache
   

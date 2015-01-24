b11e56c7b5c85b122aafa5d6fdbd4bb77c25cb82
slimshayney <slim_shayney@yahoo.com>
2015-01-24 21:26:34 +0200

    version2

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,38 @@
-## Shayne's Program
-## Put comments here that give an overall description of what your
-## functions do
+## This function (makeCacheMatrix) will return the inverse of a matrix stored in another function (cacheSolve)
+## The below 2x2 matrix was created to test the function 
+## x <- matrix(c(3, 2, -7, -5), nrow = 2, ncol = 2)
+## The inverse of the above matrix can be computed as follows:
+## solve(x)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ## This function sets the value of the matrix and the inverse
+  z <- NULL
+  set <- function(y) {
+    x <<- y
+    z <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) z <<- inverse
+  getinverse <- function() z
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  z <- x$getinverse() 
+  if(!is.null(z)) {
+    message("getting cached data.")
+    return(z)
+  }
+  data <- x$get()
+  z <- solve(data)
+  x$setinverse(z)
+  z
 }
+
+To initiate the experiment type:
+
+x <- matrix(c(3, 2, -7, -5), nrow = 2, ncol = 2)
+m = makeCacheMatrix(x)
+m$get()
+
+cacheSolve(m)
+  
